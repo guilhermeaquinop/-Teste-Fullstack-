f3ea441caf055d616930b0e9ea82910d41aea07b
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2021-11-19 01:14:33 -0300

    arquivos .php e .css

---
+++ Projeto/index.php
@@ -0,0 +1,76 @@
+<?php /* Código API */ 
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+	CURLOPT_URL => "https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"x-rapidapi-host: the-cocktail-db.p.rapidapi.com",
+		"x-rapidapi-key: 09d8e86b69msh367d3917d7092d8p152d35jsnbed02402c430"
+	],
+]);
+
+$response = (curl_exec($curl));
+$result = json_decode($response);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>Drinks</title>
+	</head>
+<body>
+	<header class="title"> 
+		<h3>Drinks</h3>
+	</header>
+	<main class="container">
+		<div class="box-names">
+			<!-- Código PHP: Gera botões com o nome de cada Drink -->
+			<?php 
+				foreach ($result->drinks as $Drinks) {
+			?>
+					<div class="fName">
+						<input class="button-name" type="submit" value="<?= $Drinks->strDrink ?>">
+					</div>
+			<?php } ?>
+			<!-- Código PHP -->			
+		</div>
+		<div class="box-infos">
+			<!-- Código PHP: Requisição da imagens, nomes e ID dos Drinks -->
+			<?php
+				foreach ($result->drinks as $Drinks) {
+			?>
+					<div class="infos">
+						<div class="image">
+							<img src="<?= $Drinks->strDrinkThumb?>"> <!-- Imagens -->	
+						</div>
+						<div class="text">
+							<div class="name">
+								<h4>Name:</h4>
+								<p><?=$Drinks -> strDrink?></p> <!-- Nome  -->	
+							</div>
+							<div class="id">
+								<h4>Id:</h4>
+								<p><?=$Drinks -> idDrink?></p><!-- ID -->	
+							</div>
+						</div>
+					</div>
+			<?php } ?>
+			<!-- Código PHP -->	
+		</div>
+	</main>
+</body>
+</html>
